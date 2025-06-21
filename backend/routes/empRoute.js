345d57d6818e417382b5cd0a0fe7d6237cbef193
Nitish Sharma <n01727146@humber.ca>
2025-06-20 21:34:22 -0400

    Delete backend directory

--- backend/routes/empRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { empList } from "../controllers/empController.js";
-
-const empRouter = express.Router()
-
-empRouter.get('/list', empList)
-
-export default empRouter
